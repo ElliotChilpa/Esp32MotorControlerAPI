6f31f376718301eae1d3840f6bda06f879a3c36d
Elliot Chilpa <elliotallan94@gmail.com>
2024-05-29 08:29:02 -0600

    Este codigo lo realizo el Mau y Funciona al 100%

--- esp8266ServerLED/esp8266ServerLED.ino
+++ esp8266ServerLED/esp8266ServerLED.ino
@@ -1,138 +1,204 @@
-/*
- *  This sketch demonstrates how to set up a simple HTTP-like server.
- *  The server will set a GPIO pin depending on the request
- *    http://server_ip/MotorDer will set the GPIO2 low,
- *    http://server_ip/MotorIzq will set the GPIO2 high
- *  server_ip is the IP address of the ESP8266 module, will be 
- *  printed to Serial when the module is connected.
- */
+[8:07 a.m., 29/5/2024] Mau: #include "ESP8266WiFi.h"
+#include <ESP8266HTTPClient.h>
 
- /*
- *  Nos Puede servir el GPIO 1 que es TX en la plac
- *  Nos Puede servir el GPIO 3 que es RX
- *  Nos Puede servir el GPIO 15 que es D8 en la placa
- *  Nos Puede servir el GPIO 13 que es el D7
- *  Nos Puede servir el GPIO 12 que es el D6
- *  Nos Puede servir el GPIO 14 que es el D5
- */
-
-#include <ESP8266WiFi.h>
-
-const char* ssid = "Totalplay-C4A5";
-const char* password = "C4A5E9782q3g346Q";
-
-
-
-// Creamos una instancia para el servidor
-// con server especificamoes el puerto 
+const char* wifiName     = "Wi-Fi IPN";
+const char* wifiPass = "";
+int contconexion = 0;
+//------------------Servidor Web en puerto 80---------------------
 WiFiServer server(80);
 
-void setup() {
-  Serial.begin(115200);
-  delay(1500);
+//---------------------VARIABLES GLOBALES-------------------------
 
-  // prepare GPIO2
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
+String header; // Variable para guardar el HTTP request++++++++++
+String estadoSalida = "off";
+const int salida = 2;
+char mensaje[50];
+//---------------------------SETUP--------------------------------
 
-  pinMode(1, OUTPUT);
-  digitalWrite(1, LOW);
 
-  pinMode(3, OUTPUT);
-  digitalWrite(3, LOW);
-  
-  // Connect to WiFi network
+void setup() {  
+    pinMode(salida, OUTPUT); 
+    Serial.begin(115200);
+    delay(10);
+  // We start by connecting to a WiFi network
   Serial.println();
-  Serial.println();
+
   Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(500);
-    Serial.print(".");
-  }
+  Serial.println(wifiName);
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
-
-  // Print the IP address
-  Serial.println(WiFi.localIP());
+  WiFi.begin(wifiName, wifiPass);
+  while (WiFi.status() != WL_CONNECTED and contconexion <50) { 
+      ++contconexion;
+      delay(500);
+      Serial.print(".");
+    }
+    if (contconexion <50) {
+        Serial.println("");
+        Serial.println("WiFi conectado");
+        Serial.println(WiFi.localIP());
+        server.begin(); // iniciamos el servidor
+    }
+    else { 
+        Serial.println("");
+        Serial.println("Error de conexion");
+    }
 }
 
-void loop() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
-  
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-  
-  // Match the request
-  int val;
-  int derM1;
-  int izqM1;
-  if (req.indexOf("/der") != -1)
-  {
-    val = 0;
-    derM1 = 0;
-    izqM1 = 1;
-  }
-  else if (req.indexOf("/izq") != -1)
-  {
-    val = 1;
-    derM1 = 1;
-    izqM1 = 0;
-  }
-  /*else if (req.indexOf("/Para") != -1)
-  }
-    val = 0;
-    derM1 = 0;
-    izqM1 = 0;
-  }*/
-  else 
-  {
-    Serial.println("invalid request");
+//----------------------------LOOP----------------------------------
+
+void loop(){
+  WiFiClient client = server.available();   // Escucha a los clientes entrantes
+  if (client) {                             // Si se conecta un nuevo cliente
+    Serial.println("New Client.");          // 
+    String currentLine = "";
+    
+    while (client.connected()) {            // loop mientras el cliente está conectado
+      if (client.available()) {             // si hay bytes para leer desde el cliente
+        char c = client.read();             // lee un byte
+        Serial.write(c);                    // imprime ese byte en el monitor serial
+        header += c;
+        if (c == '\n') {                    // si el byte es un caracter de salto de linea
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+          
+            // enciende y apaga el GPIO
+            if (header.indexOf("GET /apagar") >= 0) {
+              Serial.println("GPIO encendido");
+                digitalWrite(2, LOW); //HIGH ES 0 Y LOW ES 255
+
+            } else if (header.indexOf("GET /encender") >= 0) {
+              Serial.println("GPIO apagado");
+                digitalWrite(2, HIGH); //HIGH ES 0 Y LOW E  S 255
+            }
+
+            // la respuesta HTTP temina con una linea en blanco
+            client.println();
+            break;
+          } else { // si tenemos una nueva linea limpiamos currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // si C es distinto al caracter de retorno de carro
+          currentLine += c;      // lo agrega al final de currentLine
+        }
+      }
+    }
+    // Limpiamos la variable header
+    header = "";
+    // Cerramos la conexión
     client.stop();
-    return;
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
+}
+[8:28 a.m., 29/5/2024] Mau: #include "ESP8266WiFi.h"
+#include <ESP8266HTTPClient.h>
 
-  // Set GPIO2 according to the request
-  digitalWrite(2, val);
-  
-  // Pines puestos por Elliot Chilpa
-  digitalWrite(1, derM1);
-  digitalWrite(3, izqM1);
+const char* wifiName     = "Wi-Fi IPN";
+const char* wifiPass = "";
+int contconexion = 0;
+//------------------Servidor Web en puerto 80---------------------
+WiFiServer server(80);
 
-  client.flush();
+//---------------------VARIABLES GLOBALES-------------------------
 
-  // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
-  s += (val)?"high":"low";
-  s += "</html>\n";
+String header; // Variable para guardar el HTTP request++++++++++
+String estadoSalida = "off";
+const int salida = 2;
+const int salidaServo = 15;
+const int salidaServo2 = 13;
+char mensaje[50];
+//---------------------------SETUP--------------------------------
 
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
 
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is detroyed
+void setup() {  
+    pinMode(salida, OUTPUT); 
+        pinMode(salidaServo, OUTPUT); 
+            pinMode(salidaServo2, OUTPUT); 
+    Serial.begin(115200);
+    delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+
+  Serial.print("Connecting to ");
+  Serial.println(wifiName);
+
+  WiFi.begin(wifiName, wifiPass);
+  while (WiFi.status() != WL_CONNECTED and contconexion <50) { 
+      ++contconexion;
+      delay(500);
+      Serial.print(".");
+    }
+    if (contconexion <50) {
+        Serial.println("");
+        Serial.println("WiFi conectado");
+        Serial.println(WiFi.localIP());
+        server.begin(); // iniciamos el servidor
+    }
+    else { 
+        Serial.println("");
+        Serial.println("Error de conexion");
+    }
 }
 
+//----------------------------LOOP----------------------------------
+
+void loop(){
+  WiFiClient client = server.available();   // Escucha a los clientes entrantes
+  if (client) {                             // Si se conecta un nuevo cliente
+    Serial.println("New Client.");          // 
+    String currentLine = "";
+    
+    while (client.connected()) {            // loop mientras el cliente está conectado
+      if (client.available()) {             // si hay bytes para leer desde el cliente
+        char c = client.read();             // lee un byte
+        Serial.write(c);                    // imprime ese byte en el monitor serial
+        header += c;
+        if (c == '\n') {                    // si el byte es un caracter de salto de linea
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+          
+            // enciende y apaga el GPIO
+            if (header.indexOf("GET /encender") >= 0) {
+              Serial.println("GPIO encendido");
+                digitalWrite(2, LOW); //HIGH ES 0 Y LOW ES 255
+                digitalWrite(15, LOW); //HIGH ES 0 Y LOW ES 255
+                digitalWrite(13, HIGH); //HIGH ES 0 Y LOW ES 255
+
+
+            } else if (header.indexOf("GET /apagar") >= 0) {
+              Serial.println("GPIO apagado");
+                digitalWrite(2, HIGH); //HIGH ES 0 Y LOW E  S 255
+                digitalWrite(15, HIGH); //HIGH ES 0 Y LOW ES 255
+                digitalWrite(13, LOW); //HIGH ES 0 Y LOW ES 255
+            }else if (header.indexOf("GET /detener") >= 0) {
+              Serial.println("GPIO apagado");
+                digitalWrite(2, HIGH); //HIGH ES 0 Y LOW E  S 255
+                digitalWrite(15, LOW); //HIGH ES 0 Y LOW ES 255
+                digitalWrite(13, LOW); //HIGH ES 0 Y LOW ES 255
+            }
+
+            // la respuesta HTTP temina con una linea en blanco
+            client.println();
+            break;
+          } else { // si tenemos una nueva linea limpiamos currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // si C es distinto al caracter de retorno de carro
+          currentLine += c;      // lo agrega al final de currentLine
+        }
+      }
+    }
+    // Limpiamos la variable header
+    header = "";
+    // Cerramos la conexión
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}

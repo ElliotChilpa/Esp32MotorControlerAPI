a76e658ea24e7ddfee87b72d068facd2ae63bcc5
Elliot Chilpa <elliotallan94@gmail.com>
2024-05-29 01:16:48 -0600

    Aquí ya Funciona Que gire el motor a la derecha o izquierda con /der y /izq

--- esp8266ServerLED/esp8266ServerLED.ino
+++ esp8266ServerLED/esp8266ServerLED.ino
@@ -1,10 +1,19 @@
 /*
  *  This sketch demonstrates how to set up a simple HTTP-like server.
  *  The server will set a GPIO pin depending on the request
- *    http://server_ip/gpio/0 will set the GPIO2 low,
- *    http://server_ip/gpio/1 will set the GPIO2 high
+ *    http://server_ip/MotorDer will set the GPIO2 low,
+ *    http://server_ip/MotorIzq will set the GPIO2 high
  *  server_ip is the IP address of the ESP8266 module, will be 
  *  printed to Serial when the module is connected.
+ */
+
+ /*
+ *  Nos Puede servir el GPIO 1 que es TX en la plac
+ *  Nos Puede servir el GPIO 3 que es RX
+ *  Nos Puede servir el GPIO 15 que es D8 en la placa
+ *  Nos Puede servir el GPIO 13 que es el D7
+ *  Nos Puede servir el GPIO 12 que es el D6
+ *  Nos Puede servir el GPIO 14 que es el D5
  */
 
 #include <ESP8266WiFi.h>
@@ -12,8 +21,10 @@
 const char* ssid = "Totalplay-C4A5";
 const char* password = "C4A5E9782q3g346Q";
 
-// Create an instance of the server
-// specify the port to listen on as an argument
+
+
+// Creamos una instancia para el servidor
+// con server especificamoes el puerto 
 WiFiServer server(80);
 
 void setup() {
@@ -23,6 +34,12 @@ void setup() {
   // prepare GPIO2
   pinMode(2, OUTPUT);
   digitalWrite(2, LOW);
+
+  pinMode(1, OUTPUT);
+  digitalWrite(1, LOW);
+
+  pinMode(3, OUTPUT);
+  digitalWrite(3, LOW);
   
   // Connect to WiFi network
   Serial.println();
@@ -32,10 +49,12 @@ void setup() {
   
   WiFi.begin(ssid, password);
   
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
   Serial.println("WiFi connected");
   
@@ -67,11 +86,28 @@ void loop() {
   
   // Match the request
   int val;
-  if (req.indexOf("/gpio/0") != -1)
+  int derM1;
+  int izqM1;
+  if (req.indexOf("/der") != -1)
+  {
     val = 0;
-  else if (req.indexOf("/gpio/1") != -1)
+    derM1 = 0;
+    izqM1 = 1;
+  }
+  else if (req.indexOf("/izq") != -1)
+  {
     val = 1;
-  else {
+    derM1 = 1;
+    izqM1 = 0;
+  }
+  /*else if (req.indexOf("/Para") != -1)
+  }
+    val = 0;
+    derM1 = 0;
+    izqM1 = 0;
+  }*/
+  else 
+  {
     Serial.println("invalid request");
     client.stop();
     return;
@@ -80,6 +116,10 @@ void loop() {
   // Set GPIO2 according to the request
   digitalWrite(2, val);
   
+  // Pines puestos por Elliot Chilpa
+  digitalWrite(1, derM1);
+  digitalWrite(3, izqM1);
+
   client.flush();
 
   // Prepare the response
